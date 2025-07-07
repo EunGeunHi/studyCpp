8a310c17003dd4a2ae9bd00ff96d12cad018d014
Kim Chang Jun <kcj980@naver.com>
2025-07-07 16:18:11 +0900

    201스택: 선언

--- Baekjoon Algorithm Basic/200_DataStructure/스택.cpp
+++ Baekjoon Algorithm Basic/200_DataStructure/스택.cpp
@@ -11,16 +11,65 @@ top		: top의 정수 출력 없다면 -1출력
 #include <string>
 using namespace std;
 enum StackFunction{ PUSH,POP,SIZE,EMPTY,TOP };
+class MyStack {
+private:
+	int* data;
+	int size;
+public:
+	MyStack(int n) {
+		data = new int[n];
+		size = 0;
+	}
+	~MyStack() {
+		delete[] data;
+	}
+	int push(int);
+	int pop();
+	int getSize();
+	int top();
+	bool isEmpty();
+};
+bool isSeveral(const string&);
+void parsing(string&, int&);
+StackFunction toStackFunction(string&);
 
 int main() {
 
 	int num_commands;
 	cin >> num_commands;
+	MyStack st(num_commands);
 
-	cin.ignore();	//이전 입력 버퍼 정리
 	for (int i = 0; i < num_commands; ++i) {
 		string command;
+		int command_parsed;
+
+		cin.ignore();	//이전 입력 버퍼 정리
 		getline(cin, command);
+
+		if (isSeveral(command)) {
+			parsing(command, command_parsed);
+		}
+
+		switch (toStackFunction(command)) {
+			case PUSH:
+				cout << st.push(command_parsed) <<endl;
+				break;
+			case POP:
+				cout << st.pop() << endl;
+				break;
+			case SIZE:
+				cout << st.getSize() << endl;
+				break;
+			case EMPTY:
+				cout << st.isEmpty() << endl;
+				break;
+			case TOP:
+				cout << st.top() << endl;
+				break;
+			default:
+				cout << "switch default! :입력 이상" << endl;
+				break;
+		}
 	}
 
 	return 0;
@@ -41,7 +90,7 @@ int main() {
 
 스택 클래스
 변수
-int data[n]:n= 최대 크기
+int data[n]:n= 최대 크기 : 만
 int size: 탑 위치, size
 멤버함수
 void push(int)
