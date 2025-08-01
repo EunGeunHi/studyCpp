108bef8a88fcee091d9ec013a92bb1b0c0b3b458
EunGeunHi_laptop <EunGeunHi@DESKTOP-668KVCQ>
2025-08-02 00:24:57 +0900

    리스트 노드, 이터레이터 구현

--- Baekjoon Algorithm Basic/200_DataStructure/6_1큐&리스트 구현.cpp
+++ Baekjoon Algorithm Basic/200_DataStructure/6_1큐&리스트 구현.cpp
@@ -14,9 +14,43 @@ using namespace std;
 
 template<typename T>
 class List {
+private:
+	struct Node {	//데이터 담을 노드
+		T data;
+		Node* prev;
+		Node* next;
+
+		Node() : data()/*T기본생성자 호출됨*/ ,prev(null), next(null){}
+		Node(T value):  data(value), prev(null), next(null) {}
+	};
+public:	//auto가 접근할수 있도록
+	class Iterator{	//데이터 접근할 이터레이터,
+		friend class List<T>;//List클래스에서 Iterator.current접근할 수 있도록
+	private:
+		Node* current;
+	public:
+		Iterator(Node* ptr = nullptr) : current(ptr){}
+
+		Iterator& operator++() { current = current->next; return *this; }
+		Iterator& operator++(int) { Iterator temp = *this; ++(*this); return temp; }
+		Iterator& operator--() { current = current->prev; return *this; }
+		Iterator& operator--(int) { Iterator temp = *this; --(*this); return temp; }
+		T& operator*() { return current->data; }
+		bool operator==(const Iterator& other) const { return current == other.current; }
+		bool operator!=(const Iterator& other) const { return current != other.current; }
+	};
+//리스트 멤버 변수
+private:
+	Node* head;
+	Node* tail;
+	//Node* fin;	//List.end()에서 반환할 iterator위해서->더미노드로 
+	size_t size;	//stl에서 size는 size_t타입
+public:
+	List(){}
 
 };
 
+//큐 구현
 template<typename T>
 class Queue {
 private:
