ca9b926ac205fb46b554a935fbced97eba89e222
EunGeunHi_laptop <EunGeunHi@DESKTOP-668KVCQ>
2025-08-02 19:03:24 +0900

    6_1 리스트 구현

--- Baekjoon Algorithm Basic/200_DataStructure/6_1큐&리스트 구현.cpp
+++ Baekjoon Algorithm Basic/200_DataStructure/6_1큐&리스트 구현.cpp
@@ -20,8 +20,8 @@ private:
 		Node* prev;
 		Node* next;
 
-		Node() : data()/*T기본생성자 호출됨*/ ,prev(null), next(null){}
-		Node(T value):  data(value), prev(null), next(null) {}
+		Node() : data()/*T기본생성자 호출됨*/ ,prev(nullptr), next(nullptr){}
+		Node(T value):  data(value), prev(nullptr), next(nullptr) {}
 	};
 public:	//auto가 접근할수 있도록
 	class Iterator{	//데이터 접근할 이터레이터,
@@ -30,6 +30,7 @@ public:	//auto가 접근할수 있도록
 		Node* current;
 	public:
 		Iterator(Node* ptr = nullptr) : current(ptr){}
+		//이터레이터 복사 생성자 필요 -> 매개변수로 사용하기 위해서 -> 디폴트로도 충분
 
 		Iterator& operator++() { current = current->next; return *this; }
 		Iterator& operator++(int) { Iterator temp = *this; ++(*this); return temp; }
@@ -38,16 +39,70 @@ public:	//auto가 접근할수 있도록
 		T& operator*() { return current->data; }
 		bool operator==(const Iterator& other) const { return current == other.current; }
 		bool operator!=(const Iterator& other) const { return current != other.current; }
+	
 	};
 //리스트 멤버 변수
+// 헤드 테일 더미노드로 엣지케이스 처리 용이화
 private:
 	Node* head;
 	Node* tail;
-	//Node* fin;	//List.end()에서 반환할 iterator위해서->더미노드로 
-	size_t size;	//stl에서 size는 size_t타입
-public:
-	List(){}
+	size_t list_size;
 
+public:
+	List() {
+		head = new Node();
+		tail = new Node();
+		head->next = tail;
+		tail->prev = head;
+		list_size = 0;
+	}
+	Iterator begin() { return Iterator(head->next()); }
+	Iterator end() { return Iterator(tail()); }
+	bool empty() { return list_size == 0; }
+	size_t size() { return list_size; }
+	Iterator insert(Iterator position, const T& new_value) {
+		Node* pos = position.current;
+		Node* newNode = new(new_value, pos->prev, pos);
+		pos->prev->next = newNode;
+		pos->prev = newNode;
+		++list_size;
+		return Iterator(newNode);
+	}
+	Iterator erase(Iterator position) {
+		if (position.current == head || position.current == tail) {
+			throw std::out_of_range("Cannot erase dummy nodes or invalid iterator");
+		}
+		Node* deletedNode = position.current;
+		deletedNode->prev->next = deletedNode->next;
+		deletedNode->next->prev = deletedNode->prev;
+
+		Node* nextNode = deletedNode->next;
+		delete deletedNode;
+		--list_size;
+		return Iterator(nextNode);
+	}
+	void push_back(const T& new_value) { this->insert(--this->end(), new_value); }
+	void push_front(const T& new_value) { this->insert(this->begin(), new_value); }
+	void pop_back(){
+		if (empty()) throw std::out_of_range("pop_back() on empty list");
+		this->erase(--this->end());
+	}
+	void pop_front(){
+		if (empty()) throw std::out_of_range("pop_front() on empty list");
+		this->erase(this->begin());
+	}
+	void clear(){
+		Iterator it = begin();
+		while (it != end()) {
+			it = erase(it); // erase는 삭제된 요소의 다음 이터레이터를 반환
+		}
+	}
+
+	~List() {
+		clear();
+		delete head;
+		delete tail;
+	}
 };
 
 //큐 구현
